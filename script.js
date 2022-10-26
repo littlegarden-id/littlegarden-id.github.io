dacafcfeed6bc02269434adf0a8ea8dee614fe22
ilham <116426221+littlegarden-id@users.noreply.github.com>
2022-10-26 23:10:44 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// mulai - mengarahkan link dari icon
+
+document.getElementById("klik").onclick = function () {
+  myFunction();
+};
+
+function myFunction() {
+  window.open("http://www.instagram.com/il.hamn");
+}
