66451220bdfb27bab2c5a9c0a3de20456c0ea4c5
littlegarden-id <91ilham@gmail.com>
2026-08-10 13:57:36 +0700

    update layout baru

--- script.js
+++ script.js
@@ -1,9 +1,53 @@
-// mulai - mengarahkan link dari icon
+// drop down menu
 
-document.getElementById("klik").onclick = function () {
-  myFunction();
-};
+const dropDown = document.querySelector(".dropdown");
+const dropDownLinks = document.querySelectorAll(".dropdown .links a")
 
-function myFunction() {
-  window.open("http://www.instagram.com/il.hamn");
+function hamburg(){
+    dropDown.classList.add("active");    
 }
+
+function cancel(){
+    dropDown.classList.remove("active");
+}
+
+if (dropDownLinks.length > 0){
+    Array.from(dropDownLinks).forEach(link => {
+        link.addEventListener("click", cancel);
+    });
+}
+
+// mengisi typewriter
+const words = ["Site Civil Engineer", "Drafter Engineer", "Tech Explorer", "Subtitle Creator"];
+const textElement = document.querySelector(".typewriter-text");
+
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+const typeSpeed = 100;
+const deleteSpeed = 50;
+const pauseTime = 1000;
+
+function type(){
+    const currentWord = words[wordIndex];
+    if (isDeleting){
+        textElement.textContent = currentWord.substring(0, charIndex - 1);
+        charIndex--;
+    } else{
+        textElement.textContent = currentWord.substring(0, charIndex + 1);
+        charIndex++;
+    }
+    let currentSpeed = isDeleting ? deleteSpeed : typeSpeed;
+    if(!isDeleting && charIndex === currentWord.length){
+        currentSpeed = pauseTime;
+        isDeleting = true;
+    }
+    else if (isDeleting && charIndex === 0){
+        isDeleting = false;
+        wordIndex = (wordIndex + 1) % words.length;
+        currentSpeed = 500;
+    }
+    setTimeout(type, currentSpeed);
+}
+
+type();
